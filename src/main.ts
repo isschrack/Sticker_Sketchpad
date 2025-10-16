62007c044a520c0f6835ae61d8f3c057ee60a30b
Izzy Schrack <superizzy@outlook.com>
2025-10-16 23:20:38 +0000

    can now draw on the canvas using mouse events

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,31 @@ document.body.innerHTML = `
 `;
 
 const canvas = document.getElementById("sketchpad") as HTMLCanvasElement;
+canvas.width = 256;
+canvas.height = 256;
+document.body.append(canvas);
+const ctx = canvas.getContext("2d")!;
+const clearBtn = document.getElementById("clearBtn") as HTMLButtonElement;
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+  cursor.active = true;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  cursor.active = false;
+});
